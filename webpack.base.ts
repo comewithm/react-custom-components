2914312ea2000bffc252c0481804abf72db3e428
wanghuan1 <wanghuan1@duiba.com.cn>
2023-05-04 22:44:36 +0800

    build(webpack): less配置

--- webpack.base.ts
+++ webpack.base.ts
@@ -22,6 +22,14 @@ const baseConfig: Configuration = {
             {
                 test: /.css$/,
                 use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /.less$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "less-loader"
+                ]
             }
         ],
     },
